16eefd39705be288bf16cd70aff920145afa9881
= <kcchief@yahoo.com>
2021-10-04 16:28:07 -0500

    The coding for the challenge

--- Multiples3And5/Multiples3And5/Program.cs
+++ Multiples3And5/Multiples3And5/Program.cs
@@ -10,7 +10,22 @@ namespace Multiples3And5
     {
         static void Main(string[] args)
         {
+            //Variable int total to store the total
+            int total = 0;
 
-        }
-    }
-}
+            //Loop from 1 to 1000
+            for (int i = 1; i < 1000; i++)
+            {
+                //If multiple of 3 or 5 add to the total
+                if (i % 3 == 0 || i % 5 == 0)
+                {
+                    //Add to the total
+                    total += i;
+                }
+            }
+
+            //Display the total
+            Console.WriteLine(total);
+        }//End Main
+    }// End Class
+ }// End Name Space
